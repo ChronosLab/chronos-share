92575b168aaac5a7b2b7650de72e4a54b54d3e76
Eddie Hsu <aoe102198@gmail.com>
2024-03-23 21:05:20 +0800

    Update components and add assets folder

--- src/app/components/content.tsx
+++ src/app/components/content.tsx
@@ -8,14 +8,13 @@ interface ContentProps {
 const Content: React.FC<ContentProps> = ({ className }) => {
   return (
     <div className={`flex flex-col w-full border-opacity-50 ${className}`}>
-      <div className="grid h-20 card bg-base-300 rounded-box place-items-center">
+      <div className="grid h-200 card bg-base-500 rounded-box place-items-center flex-grow">
         <Carousel />
       </div>
-      <div className="divider">
-        =============================== Latest Share
-        ===============================
+      <div className="h-50 divider">
+        ===================== Latest Share =====================
       </div>
-      <div className="grid h-20 card rounded-box place-items-center">
+      <div className="grid h-200 card rounded-box place-items-center flex-grow">
         <TimeLine />
       </div>
     </div>
