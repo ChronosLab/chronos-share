7e32e76a5dab9202c98135d52490c698bd50ba2b
Evis Chang <yschang0926@outlook.com>
2024-03-23 22:12:00 +0800

    add mint feature

---
+++ src/app/commons/mint.test.ts
@@ -0,0 +1,8 @@
+import { mintNFT } from "./mint";
+
+describe('mint feature', () => {
+    it('mint NFT', async () => {
+        const nft = await mintNFT('Test', 'repository-images.githubusercontent.com/234710484/cdcc7000-3a0b-11ea-8f64-d9123b143b28');
+        console.log(nft);
+    });
+});
