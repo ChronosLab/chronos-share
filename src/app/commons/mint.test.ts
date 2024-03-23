4c585cbf82b7ab3d94ff2256f190ac2991b27f67
Evis Chang <yschang0926@outlook.com>
2024-03-24 00:15:03 +0800

    minor fix mint feature

--- src/app/commons/mint.test.ts
+++ src/app/commons/mint.test.ts
@@ -2,7 +2,7 @@ import { mintNFT } from "./mint";
 
 describe('mint feature', () => {
     it('mint NFT', async () => {
-        const nft = await mintNFT('Test', 'repository-images.githubusercontent.com/234710484/cdcc7000-3a0b-11ea-8f64-d9123b143b28');
-        console.log(nft);
+        const result = await mintNFT('TEST1234', 'repository-images.githubusercontent.com/234710484/cdcc7000-3a0b-11ea-8f64-d9123b143b28');
+        expect(result).toHaveProperty('blockNumber');
     });
 });
