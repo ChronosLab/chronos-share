16818e65dd8e7996ee0dd6cf91b099bdc81f17e5
UranusLin <aka611511@gmail.com>
2024-03-23 23:28:38 +0800

    feat: format

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -32,10 +32,18 @@ const Carousel: React.FC<CarouselProps> = () => {
         </div>
       </div>
       <div className="flex justify-center w-full py-2 gap-2">
-        <a href="#item1" className="btn btn-xs">1</a>
-        <a href="#item2" className="btn btn-xs">2</a>
-        <a href="#item3" className="btn btn-xs">3</a>
-        <a href="#item4" className="btn btn-xs">4</a>
+        <a href="#item1" className="btn btn-xs">
+          1
+        </a>
+        <a href="#item2" className="btn btn-xs">
+          2
+        </a>
+        <a href="#item3" className="btn btn-xs">
+          3
+        </a>
+        <a href="#item4" className="btn btn-xs">
+          4
+        </a>
       </div>
     </div>
   );
--- src/app/components/content.tsx
+++ src/app/components/content.tsx
@@ -12,7 +12,7 @@ const Content: React.FC<ContentProps> = ({ className }) => {
         <Carousel />
       </div>
       <div className="h-50 divider">
-        <p className="text-2xl">~~  Latest Share  ~~</p>
+        <p className="text-2xl">~~ Latest Share ~~</p>
       </div>
       <div className="grid h-[750px] card rounded-box place-items-center flex-grow">
         <TimeLine />
