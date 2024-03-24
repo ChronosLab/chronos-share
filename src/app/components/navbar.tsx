423ec4424f9eb102188f75e59870e26cb5a962dc
Eddie Hsu <aoe102198@gmail.com>
2024-03-24 21:41:12 +0800

    Add RWD for navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,11 +3,55 @@ import Link from "next/link";
 const Navbar: React.FC = () => {
   return (
     <div className="navbar bg-base-100 flex-none z-10">
-      <div className="flex-1">
-        <Link href={"/"} className="btn btn-ghost text-2xl">
-          Chronos Share
-        </Link>
+      <div className="navbar-start">
+        {/* view-width < 1024px layout */}
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </div>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <a>Share !</a>
+              <ul className="p-2">
+                <li>
+                  <a>
+                    <Link href={"/market/first"}>Chronos Market</Link>
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <Link href={"/market/second"}>2ry Chronos Market</Link>
+                  </a>
+                </li>
+              </ul>
+              <li>
+                <a>
+                  <Link href={"/nft/create"}>Create</Link>
+                </a>
+              </li>
+            </li>
+            <li>
+              <a>
+                <Link href={"/wallet"}>Wallet</Link>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex-1">
+          <Link href={"/"} className="btn btn-ghost text-2xl">
+            Chronos Share
+          </Link>
+        </div>
       </div>
+
       {/* nav */}
       <div className="navbar-center hidden lg:flex w-500">
         <ul className="menu menu-horizontal px-1">
